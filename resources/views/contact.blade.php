e43035e9ff8b438184557a950a738f9f4283b333
kykok18 <fadilrizky810@gmail.com>
2026-06-06 15:32:11 +0700

    Revisi Tugas Mini 1: pakai layout blade & navbar

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,21 @@
 @extends('layouts.app')
 
-@section('title', 'Contact')
+@section('title', 'Contact Us')
 
 @section('content')
 <h1>Contact Us</h1>
 <p>Hubungi kami jika Anda memiliki pertanyaan atau masukan.</p>
-<form>
-    <input type="text" name="nama" placeholder="Nama Anda">
-    <textarea name="pesan" placeholder="Pesan Anda"></textarea>
+
+<form method="POST" action="/contact">
+    @csrf
+    <div>
+        <label>Nama:</label><br>
+        <input type="text" name="nama" placeholder="Nama Anda">
+    </div>
+    <div>
+        <label>Pesan:</label><br>
+        <textarea name="pesan" placeholder="Pesan Anda" rows="4"></textarea>
+    </div>
     <button type="submit">Kirim</button>
 </form>
 @endsection
